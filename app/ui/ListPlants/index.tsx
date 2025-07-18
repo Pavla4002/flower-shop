ba7b8ca67c0da2db22d58f57f14272b56b24f713
Павла <paha16kot@inbox.ru>
2025-07-18 13:40:24 +0300

    Список растений выставлен горизонтально

--- app/ui/ListPlants/index.tsx
+++ app/ui/ListPlants/index.tsx
@@ -22,7 +22,7 @@ function ListPlants() {
     }, []);
 
     return (
-        <div>
+        <div className={styles.listPlantsBox}>
             {
                 data ? data.map(plant => <CardPlant key={plant.id} plant={plant}/>) : ''
             }
